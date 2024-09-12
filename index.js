7e25baf47fd496e571fef1b9e8ad112f1786a57c
Youssef El Idrissi <professionalidrissi@gmail.com>
2024-09-12 15:35:18 +0000

    added files

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const app = express();
+
+const updatedAt = new Date();
+
+app.use(bodyParser.json());
+
+app.post('/cicd/github-cicd', (req, res) => {
+	const { ref } = req.body;
+
+	if (ref === 'refs/heads/main') {
+		exec('git pull origin main && pm2 restart cicd_app');
+	}
+	res.sendStatus(200);
+});
+
+app.get('/cicd/time', (req, res) => {
+	res.send(`<h1>${updatedAt}</h1>`);
+});
+
+app.listen(5000, () => {
+	console.log("Listening on Port 5000...");
+})
